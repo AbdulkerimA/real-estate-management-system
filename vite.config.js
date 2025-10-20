c79dcaac3b8ab3128e41eed5b494d1347a44fbc5
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2025-10-20 02:58:50 -0700

    function added to automatically include all .js and .css files to build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
+import path from 'path';
+
+// Helper to automatically include all .js and .css files
+function getAllFiles(dir, ext, fileList = []) {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        const fullPath = path.join(dir, file);
+        if (fs.statSync(fullPath).isDirectory()) {
+            getAllFiles(fullPath, ext, fileList);
+        } else if (file.endsWith(ext)) {
+            fileList.push(fullPath.replace(/\\/g, '/'));
+        }
+    });
+    return fileList;
+}
+
+const jsFiles = getAllFiles('resources/js', '.js');
+const cssFiles = getAllFiles('resources/css', '.css');
+
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                 ...jsFiles,
+                ...cssFiles,
+            ],
             refresh: true,
         }),
         tailwindcss(),
