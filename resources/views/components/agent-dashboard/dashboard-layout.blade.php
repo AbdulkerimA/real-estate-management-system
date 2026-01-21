26becf34c5a8d35bf84db63aad1fb398c890a673
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2026-01-20 20:01:28 -0800

    messages sidebar removed

--- resources/views/components/agent-dashboard/dashboard-layout.blade.php
+++ resources/views/components/agent-dashboard/dashboard-layout.blade.php
@@ -56,11 +56,11 @@
                         </svg>
                 </x-agent-dashboard.side-bar-buttons>
 
-                <x-agent-dashboard.side-bar-buttons href="/dashboard/messages" title="Messages" message='13' class="{{ request()->is('dashboard/messages*') ? 'active' : '' }}">
+                {{-- <x-agent-dashboard.side-bar-buttons href="/dashboard/messages" title="Messages" message='13' class="{{ request()->is('dashboard/messages*') ? 'active' : '' }}">
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
                         </svg>
-                </x-agent-dashboard.side-bar-buttons>
+                </x-agent-dashboard.side-bar-buttons> --}}
 
                 <x-agent-dashboard.side-bar-buttons href="/dashboard/earnings" title="Earnings" class="{{ request()->is('dashboard/earnings*') ? 'active' : '' }}">
                     <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
